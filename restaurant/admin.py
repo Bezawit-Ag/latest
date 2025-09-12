d8c1da4a4d455d8b40eb236c83e68b12910622ee
(dd) <bezawitagumas2@gmail.com>
2025-09-12 20:47:43 +0100

    footer

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Menu
+from .models import Booking
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'guest_number', 'comment')
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('name', 'Price')   
 
 admin.site.register(Menu, MenuAdmin)
+admin.site.register(Booking, BookingAdmin)
 
 
 # Register your models here.
